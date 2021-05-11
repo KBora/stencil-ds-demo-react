7ef0875e9584eea5054f3ed5c35b28623b7ca088
wookkii <wookkii@home>
2021-05-12 09:50:03 +1000

    Define custom elements and display web component

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <twds-hexagram lines="111111"></twds-hexagram>
+
+      <twds-hexagram lines="001111"></twds-hexagram>
+
+      <twds-hexagram lines="101000"></twds-hexagram>
+
+      <twds-hexagram lines="000000"></twds-hexagram>
+
     </div>
   );
 }
